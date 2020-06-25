879c16e7feadcf865401900db2d0853e9fd327ba
Hao @GMS <haotv0802@gmail.com>
2020-06-25 14:41:08 +0700

    Initiate the files.

---
+++ src/main/java/com/nabsys/UserRepository.java
@@ -0,0 +1,16 @@
+package com.nabsys;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+/**
+ * Created by haoho on 6/24/20.
+ */
+@Repository
+@Transactional
+public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findAllByAtk(int atk);
+}
---
+++ src/test/java/com/nabsys/NabsysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nabsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NabsysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
