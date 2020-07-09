436e9ca6a1ca3faf3e55262728463851ca35de14
haoho <hoanhhao@gmail.com>
2020-07-09 13:48:07 +0700

    Update SQL.

--- src/main/resources/sql.sql
+++ src/main/resources/sql.sql
@@ -1,5 +1,8 @@
-CREATE DATABASE haoho;
+DROP DATABASE IF EXISTS `haoho`;
+CREATE DATABASE IF NOT EXISTS `haoho`;
 use haoho;
+
+DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`
 (
     `id`      bigint(20) NOT NULL AUTO_INCREMENT,
@@ -11,6 +14,27 @@ CREATE TABLE `user`
     PRIMARY KEY (`id`)
 );
 
+DROP TABLE IF EXISTS `product`;
+CREATE TABLE `product`
+(
+    `id`      bigint(20)  NOT NULL AUTO_INCREMENT,
+    `name`    int         NULL DEFAULT NULL,
+    `price`   int         NULL DEFAULT NULL,
+    `branch`  int         NULL DEFAULT NULL,
+    `colour`  varchar(20) NULL DEFAULT NULL,
+    `created` datetime         DEFAULT now() NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
+DROP TABLE IF EXISTS `order`;
+CREATE TABLE `order`
+(
+    `id`         bigint(20)             NOT NULL AUTO_INCREMENT,
+    `product_id` bigint                 NOT NULL,
+    `created`    datetime DEFAULT now() NOT NULL,
+    CONSTRAINT `order_product_id` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`),
+    PRIMARY KEY (`id`)
+);
 
 DELIMITER $$
 CREATE PROCEDURE generate_data()
